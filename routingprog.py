f56c414076d0f8ee8e66e7672f8d3cc6541488ea
Kush Darji <60727944+Palpeleno@users.noreply.github.com>
2023-11-27 21:50:24 -0500

    Create routingprog.py

---
+++ routingprog.py
@@ -0,0 +1,23 @@
+Class Node: 
+    def __init__(self. node_id):
+        self.node_id = node_id
+        self.neighbors = {}
+        self.routing_table = {}
+    
+    def add_neighbor(self,neighbor_id, cost):
+        
+    def update_routing_table(self, destination, next_hop, path_cost):
+        
+class Link:
+    def __init__(self, node1, node2, cost):
+        
+class Topology:
+    def __init__(self):
+        self.nodes = {}
+        self.links = []
+    
+    def add_node()
+
+    def add_link(self, node1, node2, cost):
+        
+
